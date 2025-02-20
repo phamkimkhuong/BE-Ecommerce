a160e10943b4debec3b25fa2a2e1ce5dbc49a252
phamkimkhuong <phamkhuong436@gmail.com>
2025-02-20 22:19:52 +0700

    create and setup api-gateway

--- user_service/src/main/java/iuh/fit/se/UserServiceApplication.java
+++ user_service/src/main/java/iuh/fit/se/UserServiceApplication.java
@@ -1,15 +1,13 @@
 package iuh.fit.se;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	private final static Logger logger = LoggerFactory.getLogger(UserServiceApplication.class.getName());
