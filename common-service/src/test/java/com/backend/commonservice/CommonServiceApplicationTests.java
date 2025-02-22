5464b374ad75b3b63e21d1159c464dbce2287ae9
phamkimkhuong <phamkhuong436@gmail.com>
2025-02-22 09:12:13 +0700

    create common-service share library

---
+++ common-service/src/test/java/com/backend/commonservice/CommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.commonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
