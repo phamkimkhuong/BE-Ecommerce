fa1916624ba4244ea24cef5ad3bda54def4a62aa
phamkimkhuong <phamkhuong436@gmail.com>
2025-05-20 13:55:12 +0700

    add script auto deploy

---
+++ sripts/deploy.sh
@@ -0,0 +1,21 @@
+
+# shellcheck disable=SC2034
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Deploying with branch: $BRANCH"
+
+# cd on directory source code
+cd app/Web-Ecommerce || exit 1
+# Get all branches
+git fetch -a
+# Checkout main branch
+git checkout $BRANCH
+# Pull the latest changes
+git pull
+# down container
+docker compose -f docker-compose.yml down
+# pull images latest
+docker compose -f docker-compose.yml pull
+# build images
+docker compose -f docker-compose.yml up -d
+# clean up
+docker system prune -af
