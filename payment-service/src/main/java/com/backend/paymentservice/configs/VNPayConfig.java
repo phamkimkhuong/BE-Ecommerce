b00f04aea8532abe42caaa18db393f4605ba7366
phamkimkhuong <phamkhuong436@gmail.com>
2025-05-22 15:07:04 +0700

    add docker swarm

--- payment-service/src/main/java/com/backend/paymentservice/configs/VNPayConfig.java
+++ payment-service/src/main/java/com/backend/paymentservice/configs/VNPayConfig.java
@@ -29,7 +29,7 @@ public class VNPayConfig {
     public static String vnp_HashSecret = "CXVOERDOGSLQV1R0P3KNBGQ1TJBCME1V";
     public static String vnp_Version = "2.1.0";
     public static String vnp_Command = "pay";
-    public static String vnp_ReturnUrl = "http://34.51.23.249:8080/api/payment/vn-pay/payment-info";
+    public static String vnp_ReturnUrl = "http://103.112.210.245:8080/api/payment/vn-pay/payment-info";
 
     public static String hmacSHA512(final String key, final String data) {
         try {
---
+++ sripts/deploy-swram.sh
@@ -0,0 +1,42 @@
+# shellcheck disable=SC2034
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+echo "Deploying with branch: $BRANCH"
+
+# cd on directory source code
+cd app/BE-Ecommerce || exit 1
+
+# Get all branches
+git fetch -a
+
+# Checkout main branch
+git checkout $BRANCH
+
+# Pull the latest changes
+git pull
+
+# ===== DOCKER SWARM DEPLOYMENT =====
+# Tên stack
+STACK_NAME="ecommerce-stack"
+
+# 2. REMOVE STACK CŨ (nếu có)
+echo "Removing old stack..."
+docker stack rm $STACK_NAME || true
+
+# Đợi stack được remove hoàn toàn
+echo "Waiting for stack to be completely removed..."
+sleep 10
+
+# down container
+docker-compose -f docker-compose-swarm.yml down
+# pull images latest
+docker-compose -f docker-compose-swarm.yml pull
+
+# 3. DEPLOY STACK MỚI
+echo "Deploying new stack..."
+docker stack deploy -c docker-compose-swarm.yml $STACK_NAME
+
+# 4. KIỂM TRA STATUS
+echo "Checking deployment status..."
+docker stack services $STACK_NAME
+
+docker system prune -f
